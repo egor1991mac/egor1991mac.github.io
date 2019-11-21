e080d2500d0240632af409f838097bba87f50aeb
Егор <egor1991mac@gmail.com>
2019-11-21 20:14:24 +0300

    123

--- project/dist/js/bundle.js
+++ project/dist/js/bundle.js
@@ -18989,6 +18989,7 @@ $(function () {
       document.addEventListener('click', function (e) {
         if ($(e.target).hasClass('custom-file-input')) {
           bs_custom_file_input__WEBPACK_IMPORTED_MODULE_3___default.a.init();
+          console.log($(e.target).parent('.custom-file'));
           $(e.target).parent('.custom-file').find('.reset').toggleClass('active');
         }
       });
