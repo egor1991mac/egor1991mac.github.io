9551de04a859ab07f4bd10c4dd6315012f6bb177
Егор <egor1991mac@gmail.com>
2019-11-21 20:12:27 +0300

    123

--- project/dist/js/bundle.js
+++ project/dist/js/bundle.js
@@ -18998,10 +18998,10 @@ $(function () {
         $(this).parent().addClass("active");
         var $this = $(this);
         $(this).parent().find(".reset").click(function (e) {
-          console.log($this, _this, e.currentTarget);
-          console.log(e.currentTarget.closest('.custom-file')); //('.custom-file').querySelector('.reset').classList.remove('active');
+          //console.log($this,_this,e.currentTarget);
+          // console.log(e.currentTarget.closest('.custom-file'))
+          //('.custom-file').querySelector('.reset').classList.remove('active');
           //$(this).removeClass("active");
-
           $(this).parent().removeClass("active");
           $(this).parent().find("input").val("");
           $this.text("");
